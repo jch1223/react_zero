7c12a785d3f54d29d77a2b39bf33b8645727124e
jch1223 <ico1828@gmail.com>
2020-04-07 20:37:10 +0900

    웹팩 설정

---
+++ 2. 끝말잇기/WordRelay.jsx
@@ -0,0 +1,10 @@
+const React = require("react");
+const { Component } = React;
+
+class WordRelay extends Component {
+  state = {};
+
+  render() {}
+}
+
+module.exports = WordRelay;
--- 2. 끝말잇기/webpack.config.js
+++ 2. 끝말잇기/webpack.config.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+  name: "wordrelay-setting",
+  mode: "development", // 실서비스 : production
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  entry: {
+    app: ["./client"],
+  }, //입력
+  ouput: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  }, //출력
+};
