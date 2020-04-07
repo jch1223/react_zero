d2af1daa05a28a74607249685f31b18c29b13fab
jch1223 <ico1828@gmail.com>
2020-04-08 04:16:17 +0900

    끝말 잇기

--- 2. 끝말잇기/WordRelay.jsx
+++ 2. 끝말잇기/WordRelay.jsx
@@ -2,9 +2,45 @@ const React = require("react");
 const { Component } = React;
 
 class WordRelay extends Component {
-  state = {};
+  state = {
+    word: "끝말잇기",
+    value: "",
+    result: "",
+  };
 
-  render() {}
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({ result: "딩동댕", word: this.state.value, value: "" });
+    } else {
+      this.setState({ result: "땡", value: "" });
+    }
+
+    this.input.focus();
+  };
+
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  input;
+
+  onRefInput = (ref) => {
+    this.input = ref;
+  };
+
+  render() {
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input type='text' ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+          <button type='submit'>입력!</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
+    );
+  }
 }
 
 module.exports = WordRelay;
