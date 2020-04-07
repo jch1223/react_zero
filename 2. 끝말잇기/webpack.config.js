22b68da3c1775f418525a107838c445baa5ed3eb
jch1223 <ico1828@gmail.com>
2020-04-07 21:08:12 +0900

    웹팩 빌드

--- 2. 끝말잇기/webpack.config.js
+++ 2. 끝말잇기/webpack.config.js
@@ -11,7 +11,20 @@ module.exports = {
   entry: {
     app: ["./client"],
   }, //입력
-  ouput: {
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+    ],
+  }, //entry에 적용할 것들
+  output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js",
   }, //출력
