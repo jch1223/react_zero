02a91682ea75b284ddb9cf9ec49c74ad19d55a9b
jch1223 <ico1828@gmail.com>
2020-05-04 17:57:14 +0900

    컴포넌트 최적화 pure component

--- 3.숫자야구/Try.jsx
+++ 3.숫자야구/Try.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { PureComponent } from "react";
 
-const Try = ({ tryInfo }) => {
-  return (
-    <li>
-      <div>{tryInfo.try}</div>
-      <div>{tryInfo.result}</div>
-    </li>
-  );
-};
+class Try extends PureComponent {
+  render() {
+    const { tryInfo } = this.props;
+    return (
+      <li>
+        <div>{tryInfo.try}</div>
+        <div>{tryInfo.result}</div>
+      </li>
+    );
+  }
+}
+
+// const Try = ({ tryInfo }) => {
+//   return (
+//     <li>
+//       <div>{tryInfo.try}</div>
+//       <div>{tryInfo.result}</div>
+//     </li>
+//   );
+// };
 
 export default Try;
