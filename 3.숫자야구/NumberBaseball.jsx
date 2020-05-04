d61916800792cf16ab5c3fa9bcda37dbd896bfc2
jch1223 <ico1828@gmail.com>
2020-05-04 15:49:40 +0900

    change hooks

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Try from "./Try";
 
 const { hot } = require("react-hot-loader/root");
@@ -8,90 +8,80 @@ function getNumbers() {
   const array = [];
 
   for (let i = 0; i < 4; i++) {
-    const chosen = candidate.splice(Math.floor(Math.random() * (9 - 1)), 1)[0];
+    const random = Math.random() * candidate.length;
+
+    const chosen = candidate.splice(random, 1)[0];
     array.push(chosen);
   }
-
   return array;
 }
 
-class NumberBaseball extends Component {
-  state = {
-    result: "",
-    value: "",
-    answer: getNumbers(),
-    tries: [],
-  };
+const NumberBaseball = () => {
+  const [result, setResult] = useState("");
+  const [value, setValue] = useState("");
+  const [answer, setAnswer] = useState(getNumbers());
+  const [tries, setTries] = useState([]);
 
-  onSubmitForm = (e) => {
+  const onSubmitForm = (e) => {
     e.preventDefault();
-    if (this.state.value === this.state.answer.join("")) {
-      this.setState({
-        result: "홈런",
-        tries: [...this.state.tries, { try: this.state.value, result: "홈런" }],
+    if (value === answer.join("")) {
+      setResult("홈런");
+      setTries((prevTries) => {
+        return [...prevTries, { try: value, result: "홈런" }];
       });
+
       alert("게임을 다시 시작합니다");
 
-      this.setState({
-        value: "",
-        answer: getNumbers(),
-        tries: [],
-      });
+      setValue("");
+      setAnswer(getNumbers());
+      setTries([]);
     } else {
-      const answerArray = this.state.value.split("").map((item) => parseInt(item));
+      const answerArray = value.split("").map((item) => parseInt(item));
       let strike = 0;
       let ball = 0;
 
-      if (this.state.tries.length >= 9) {
-        this.setState({
-          result: `실패! 정답 : ${this.state.answer.join(",")}`,
-        });
+      if (tries.length >= 9) {
+        setResult(`실패! 정답 : ${answer.join(",")}`);
 
         alert("게임을 다시 시작합니다");
 
-        this.setState({
-          value: "",
-          answer: getNumbers(),
-          tries: [],
-        });
+        setValue("");
+        setAnswer(getNumbers());
+        setTries([]);
       } else {
         for (let i = 0; i < 4; i++) {
-          if (answerArray[i] === this.state.answer[i]) {
+          if (answerArray[i] === answer[i]) {
             strike += 1;
-          } else if (this.state.answer.includes(answerArray[i])) {
+          } else if (answer.includes(answerArray[i])) {
             ball += 1;
           }
         }
 
-        this.setState({
-          tries: [...this.state.tries, { try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다` }],
+        setTries((prevTries) => {
+          return [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다` }];
         });
       }
     }
   };
 
-  onChangeInput = (e) => {
-    this.setState({
-      value: e.target.value,
-    });
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
   };
 
-  render() {
-    return (
-      <>
-        <h1>{this.state.result}</h1>
-        <form onSubmit={this.onSubmitForm}>
-          <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
-        </form>
-        <div>시도 : {this.state.tries.length}</div>
-        <ul>
-          {this.state.tries.map((item, i) => {
-            return <Try tryInfo={item} key={`${i + 1}차시도`} />;
-          })}
-        </ul>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1>{result}</h1>
+      <form onSubmit={onSubmitForm}>
+        <input maxLength={4} value={value} onChange={onChangeInput} />
+      </form>
+      <div>시도 : {tries.length}</div>
+      <ul>
+        {tries.map((item, i) => {
+          return <Try tryInfo={item} key={`${i + 1}차시도`} />;
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default hot(NumberBaseball);
