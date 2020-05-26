7b209bba9057c9d3767865641928cfdf5e32d8e5
jch <ico1828@gmail.com>
2020-05-26 17:52:16 +0900

    반응속도 체크

--- 4.반응속도체크/ResponseCheck.jsx
+++ 4.반응속도체크/ResponseCheck.jsx
@@ -7,6 +7,10 @@ class ResponseCheck extends Component {
     result: [],
   };
 
+  timeout;
+  startTime;
+  endTime;
+
   onClickScreen = () => {
     const { state, message, result } = this.state;
     if (state === "waiting") {
@@ -14,14 +18,30 @@ class ResponseCheck extends Component {
         state: "ready",
         message: "초록색이 되면 클릭하세요",
       });
-      setTimeout(() => {
+      this.timeout = setTimeout(() => {
         this.setState({
           state: "now",
           message: "지금 클릭",
         });
+        this.startTime = new Date();
       }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
     } else if (state === "ready") {
+      //게임 시작전 클릭
+      clearTimeout(this.timeout);
+      this.setState({
+        state: "waiting",
+        message: "너무 성급하시군요! 초록색이 된 후에 클릭하세요",
+      });
     } else if (state === "now") {
+      //반응속도 클릭
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: "waiting",
+          message: "클릭해서 시작하세요",
+          result: [...prevState.result, this.endTime - this.startTime],
+        };
+      });
     }
   };
 
