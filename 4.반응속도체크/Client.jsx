62e1c70adbad395ad3424a1aaf9b4ff0efc0eab0
jch <ico1828@gmail.com>
2020-05-29 11:21:13 +0900

    react hooks로 전환

--- 4.반응속도체크/Client.jsx
+++ 4.반응속도체크/Client.jsx
@@ -1,6 +1,7 @@
 const React = require("react");
 const ReactDom = require("react-dom");
 
-import ResponseCheck from "./ResponseCheck";
+// import ResponseCheck from "./ResponseCheck";
+import ResponseCheck from "./ResponseCheckHooks";
 
 ReactDom.render(<ResponseCheck />, document.querySelector("#root"));
---
+++ 4.반응속도체크/ResponseCheckHooks.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useRef } from "react";
+
+const ResponseCheckHooks = () => {
+  const [state, setState] = useState("waiting");
+  const [message, setMessage] = useState("클릭해서 시작하세요.");
+  const [result, setResult] = useState([]);
+  const timeout = useRef(null);
+  const startTime = useRef();
+  const endTime = useRef();
+
+  const onClickScreen = () => {
+    if (state === "waiting") {
+      setState("ready");
+      setMessage("초록색이 되면 클릭하세요");
+
+      timeout.current = setTimeout(() => {
+        setState("now");
+        setMessage("지금 클릭");
+
+        startTime.current = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+    } else if (state === "ready") {
+      //게임 시작전 클릭
+      clearTimeout(timeout.current);
+
+      setState("waiting");
+      setMessage("너무 성급하시군요! 초록색이 된 후에 클릭하세요");
+    } else if (state === "now") {
+      //반응속도 클릭
+      endTime.current = new Date();
+      setState("waiting");
+      setMessage("클릭해서 시작하세요");
+      setResult((prevResult) => [
+        ...prevResult,
+        endTime.current - startTime.current,
+      ]);
+    }
+  };
+
+  const onReset = () => {
+    setResult([]);
+  };
+
+  const renderAverage = () => {
+    return result.length === 0 ? null : (
+      <>
+        <div>
+          평균 시간 :{result.reduce((a, c) => a + c) / result.length}
+          ms
+        </div>
+        <button onClick={onReset}>리셋</button>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div id="screen" className={state} onClick={onClickScreen}>
+        {message}
+      </div>
+      {renderAverage()}
+    </>
+  );
+};
+
+export default ResponseCheckHooks;
