3d353b0403fbffce0fd4a5bc590a017ba3bd561e
jch1223 <ico1828@gmail.com>
2020-06-30 13:30:58 +0900

    틱택토 세팅

---
+++ 7.틱택토/TicTacToe.jsx
@@ -0,0 +1,13 @@
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useMemo,
+  useCallback,
+} from "react";
+
+const TicTacToe = () => {
+  return <></>;
+};
+
+export default TicTacToe;
---
+++ 7.틱택토/TicTacToeClass.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class TicTacToe extends Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default TicTacToe;
