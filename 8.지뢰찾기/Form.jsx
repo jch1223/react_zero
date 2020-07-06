523f911a57102db242989d4dc72df883f544811b
jch1223 <ico1828@gmail.com>
2020-07-06 10:52:23 +0900

    form component

--- 8.지뢰찾기/Form.jsx
+++ 8.지뢰찾기/Form.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 const Form = () => {
-  return <div></div>;
+  const [row, setRow] = useState(10);
+  const [cell, setCell] = useState(10);
+  const [mine, setMine] = useState(20);
+
+  const onChangeRow = useCallback((e) => {
+    setRow(e.target.value);
+  }, []);
+
+  const onChangeRow = useCallback((e) => {
+    setCell(e.target.value);
+  }, []);
+
+  const onChangeRow = useCallback((e) => {
+    setMine(e.target.value);
+  }, []);
+
+  const onClickBtn = useCallback(() => {}, []);
+
+  return (
+    <div>
+      <input
+        type="number"
+        placeholder="세로"
+        value={row}
+        onChange={onChangeRow}
+      />
+      <input
+        type="number"
+        placeholder="가로"
+        value={cell}
+        onChange={onChangeCell}
+      />
+      <input
+        type="number"
+        placeholder="지뢰"
+        value={mine}
+        onChange={onChangeMine}
+      />
+    </div>
+  );
 };
 
 export default Form;
