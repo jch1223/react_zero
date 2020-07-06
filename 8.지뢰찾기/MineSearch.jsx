fd9741aa84be2541509f1e56a3203db028b8952c
jch1223 <ico1828@gmail.com>
2020-07-06 12:02:58 +0900

    지뢰 심는 로직

--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -2,6 +2,17 @@ import React, { useReducer, createContext, useMemo } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
+export const CODE = {
+  MINE: -7,
+  NORMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPEN: 0,
+};
+
 export const TableContext = createContext({
   tableData: [],
   dispatch: () => {},
@@ -11,6 +22,43 @@ const initialState = {
   tableData: [],
   timer: 0,
   result: "",
+};
+
+const plantMine = (row, cell, mine) => {
+  console.log(row, cell, mine);
+  const candidate = Array(row * cell)
+    .fill()
+    .map((arr, i) => {
+      return i;
+    });
+
+  const shuffle = [];
+
+  while (candidate.length > row * cell - mine) {
+    const chosen = candidate.splice(
+      Math.floor(Math.random() * candidate.length),
+      1
+    )[0];
+    shuffle.push(chosen);
+  }
+
+  const data = [];
+  for (let i = 0; i < row; i++) {
+    const rowData = [];
+    data.push(rowData);
+    for (let j = 0; j < cell; j++) {
+      rowData.push(CODE.NORMAL);
+    }
+  }
+
+  for (let k = 0; k < shuffle.length; k++) {
+    const ver = Math.floor(shuffle[k] / cell);
+    const hor = shuffle[k] % cell;
+    data[ver][hor] = CODE.MINE;
+  }
+
+  console.log(data);
+  return data;
 };
 
 export const START_GAME = "START_GAME";
