31d4582112c800283e6941a6df15db6a63676a98
jch1223 <ico1828@gmail.com>
2020-07-21 17:24:55 +0900

    지뢰찾기 뼈대

--- 8.지뢰찾기/Td.jsx
+++ 8.지뢰찾기/Td.jsx
@@ -1,16 +1,15 @@
 import React, { useContext } from "react";
 import { TableContext } from "./MineSearch";
 
-const getTdStyle = (code) => {
-};
+const getTdStyle = (code) => {};
 
 const getTdText = (code) => {};
 
 const Td = ({ rowIndex, cellIndex }) => {
   const { tableData } = useContext(TableContext);
-
+  let i;
   return (
-    <td style={getTdStyle({tableData[rowIndex][cellIndex]})}>
+    <td style={getTdStyle(tableData[rowIndex][cellIndex])}>
       {getTdText(tableData[rowIndex][cellIndex])}
     </td>
   );
