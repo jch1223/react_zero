abc8c33989779b697ce576273a03ed2fa41fe224
jch1223 <ico1828@gmail.com>
2020-07-22 17:36:02 +0900

    지뢰가 아닌칸 한번에 열기

--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -21,9 +21,15 @@ export const TableContext = createContext({
 
 const initialState = {
   tableData: [],
+  data: {
+    row: 0,
+    cell: 0,
+    mine: 0,
+  },
   timer: 0,
   result: "",
   halted: true,
+  openedCount: 0,
 };
 
 export const START_GAME = "START_GAME";
@@ -38,42 +44,103 @@ const reducer = (state, action) => {
     case START_GAME:
       return {
         ...state,
+        data: {
+          row: action.row,
+          cell: action.cell,
+          mine: action.mine,
+        },
         tableData: plantMine(action.row, action.cell, action.mine),
         halted: false,
       };
     case OPEN_CELL: {
       const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      tableData[action.row][action.cell] = CODE.OPEN;
+      tableData.forEach((row, i) => {
+        tableData[i] = [...state.tableData[i]];
+      });
 
-      let around = [];
-      if (tableData[action.row - 1]) {
-        around = around.concat(
-          tableData[action.row - 1][action.cell - 1],
-          tableData[action.row - 1][action.cell],
-          tableData[action.row - 1][action.cell + 1]
-        );
-      }
-      tableData[action.row][action.cell - 1] &&
-        (around = around.concat(tableData[action.row][action.cell - 1]));
-      tableData[action.row][action.cell + 1] &&
-        (around = around.concat(tableData[action.row][action.cell + 1]));
-      if (tableData[action.row + 1]) {
-        around = around.concat(
-          tableData[action.row + 1][action.cell - 1],
-          tableData[action.row + 1][action.cell],
-          tableData[action.row + 1][action.cell + 1]
-        );
-      }
+      const checked = [];
+      let openedCount = 0;
 
-      const count = around.filter((v) =>
-        [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)
-      ).length;
-      console.log(around, count);
-      tableData[action.row][action.cell] = count;
+      const checkAround = (row, cell) => {
+        if (
+          row < 0 ||
+          row >= tableData.length ||
+          cell < 0 ||
+          cell >= tableData[0].length
+        ) {
+          return;
+        }
+        if (
+          [
+            CODE.OPEN,
+            CODE.FLAG,
+            CODE.FLAG_MINE,
+            CODE.QUESTION_MINE,
+            CODE.QUESTION,
+          ].includes(tableData[row][cell])
+        ) {
+          return;
+        }
+        if (checked.includes(row + "," + cell)) {
+          return;
+        } else {
+          checked.push(row + "," + cell);
+        }
+
+        let around = [tableData[row][cell - 1], tableData[row][cell + 1]];
+        if (tableData[row - 1]) {
+          around = around.concat(
+            tableData[row - 1][cell - 1],
+            tableData[row - 1][cell],
+            tableData[row - 1][cell + 1]
+          );
+        }
+        if (tableData[row + 1]) {
+          around = around.concat(
+            tableData[row + 1][cell - 1],
+            tableData[row + 1][cell],
+            tableData[row + 1][cell + 1]
+          );
+        }
+
+        const count = around.filter((v) =>
+          [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)
+        ).length;
+        if (count === 0) {
+          const near = [];
+          if (row - 1 > -1) {
+            near.push([row - 1, cell - 1]);
+            near.push([row - 1, cell]);
+            near.push([row - 1, cell + 1]);
+          }
+
+          near.push([row, cell - 1]);
+          near.push([row, cell + 1]);
+
+          if (row + 1 < tableData.length) {
+            near.push([row + 1, cell - 1]);
+            near.push([row + 1, cell]);
+            near.push([row + 1, cell + 1]);
+          }
+
+          near.forEach((n) => {
+            if (tableData[n[0]][n[1]] !== CODE.OPEN) {
+              checkAround(n[0], n[1]);
+            }
+          });
+        }
+        if (tableData[row][cell] === CODE.NORMAL) {
+          openedCount += 1;
+        }
+        tableData[row][cell] = count;
+      };
+
+      checkAround(action.row, action.cell);
+
       return {
         ...state,
         tableData,
+        openedCount: state.openedCount + openedCount,
       };
     }
     case CLICK_MINE: {
