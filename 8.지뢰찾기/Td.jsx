a7a4ae7f6a036842f6be6974824ed6d9248f10f7
jch-gudi <ico1828@google.com>
2020-07-22 13:22:03 +0900

    지뢰칸 렌더링

--- 8.지뢰찾기/Td.jsx
+++ 8.지뢰찾기/Td.jsx
@@ -1,13 +1,37 @@
 import React, { useContext } from "react";
-import { TableContext } from "./MineSearch";
+import { TableContext, CODE } from "./MineSearch";
 
-const getTdStyle = (code) => {};
+const getTdStyle = (code) => {
+  switch (code) {
+    case CODE.NORMAL:
+    case CODE.MINE:
+      return {
+        background: "#444",
+      };
+    case CODE.OPEN:
+      return {
+        background: "#fff",
+      };
+    default:
+      return {
+        background: "#fff",
+      };
+  }
+};
 
-const getTdText = (code) => {};
+const getTdText = (code) => {
+  switch (code) {
+    case CODE.NORMAL:
+      return "";
+    case CODE.MINE:
+      return "x";
+    case CODE.OPEN:
+    default:
+  }
+};
 
 const Td = ({ rowIndex, cellIndex }) => {
   const { tableData } = useContext(TableContext);
-  let i;
   return (
     <td style={getTdStyle(tableData[rowIndex][cellIndex])}>
       {getTdText(tableData[rowIndex][cellIndex])}
